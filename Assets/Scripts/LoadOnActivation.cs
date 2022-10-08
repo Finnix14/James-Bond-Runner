d3357245af1fd3c76e92da794db53f07ac79f19f
Finnix14 <49366542+Finnix14@users.noreply.github.com>
2022-10-08 14:20:21 +0100

    Play Game Button For Cinematic Work

--- Assets/Scripts/LoadOnActivation.cs
+++ Assets/Scripts/LoadOnActivation.cs
@@ -3,9 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class LoadOnActivation : MonoBehaviour
 {
-    public void OnEnable()
+    public void PlayGame()
     {
-        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
-        SceneManager.LoadScene("", LoadSceneMode.Additive);
+            SceneManager.LoadScene("Play Test");
+
     }
 }
