e0e1524bdae74db5210c52ec199e0842af0fb80e
finnix16 <90830613+finnix16@users.noreply.github.com>
2022-10-13 14:14:29 +0100

    Lego Death

--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Death : MonoBehaviour
 {
@@ -8,6 +9,12 @@ public class Death : MonoBehaviour
     public GameObject deathUI;
     public Animator anim;
     public AudioSource deadSound;
+
+    public Transform t;
+    public float force;
+    public float radius;
+
+    public List<GameObject> _parts;
     void Start()
     {
         pc = GetComponent<PlayerController>();
@@ -21,7 +28,10 @@ public class Death : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Obstacle"))
         {
-            anim.SetTrigger("Death");   
+            dyingFR();
+            
+            
+            // anim.SetTrigger("Death");   
             Time.timeScale = 0.25f;
            
             deathUI.gameObject.SetActive(true);
@@ -31,9 +41,21 @@ public class Death : MonoBehaviour
             deadSound.Play();
             deadSound.pitch = 0.5f;
 
+            anim.enabled = false;
+          
 
         }
 
+
+    }
+    public void dyingFR()
+    {
+        foreach (var t in _parts)
+        {
+            t.transform.SetParent(null);
+            t.AddComponent<Rigidbody>().AddExplosionForce(force, Vector3.zero, radius);
+            t.AddComponent<CapsuleCollider>();
+        }
     }
 
 
